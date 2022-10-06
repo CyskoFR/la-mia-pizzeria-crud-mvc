1418ee500a637aeb2489546f026148352d63417c
CyskoFR <franc.sko91@gmail.com>
2022-10-06 14:39:27 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using la_mia_pizzeria.Models;
+
+namespace la_mia_pizzeria.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public ActionResult Index()
+        {
+            PizzeriaContext db = new PizzeriaContext();
+
+            return View(db.Pizzas.ToList());
+        }
+
+        public ActionResult Show(int id)
+        {
+            PizzeriaContext db = new PizzeriaContext();
+
+            return View(db.Pizzas.Find(id));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Pizza formData)
+        {
+            using (PizzeriaContext context = new PizzeriaContext())
+            {
+                Pizza pizzaCreate = new Pizza();
+                pizzaCreate.Name = formData.Name;
+                pizzaCreate.Description = formData.Description;
+                pizzaCreate.Picture = formData.Picture;
+                pizzaCreate.Price = formData.Price;
+
+                context.Pizzas.Add(pizzaCreate);
+
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(50, ErrorMessage = "Il nome non può avere più di 25 caratteri")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(300, ErrorMessage = "La descrizione non può avere più di 300 caratteri")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        public string Picture { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        public double Price { get; set; }
+
+        public Pizza()
+        {
+        
+        }
+
+        public Pizza(int id, string name, string description, string picture, double price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Picture = picture;
+            Price = price;
+        }
+    }
+}
